4d6c8d12a5711ea90607fe9ba69793892ec1b36d
James Allsopp <j.allsopp@bham.ac.uk>
2023-11-27 23:21:15 +0000

    Simplified for presentation

---
+++ BasicSlurmFile/SlurmArrayJobs/1-arrayJob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --qos arc
+#SBATCH --account edmondac-rsg
+#SBATCH --time 0:0:1
+#SBATCH --job-name "SlurmArrayJob"
+#SBATCH --array=1-10%2
+
+module purge
+module load baskerville
+
+echo "This script is running on $(hostname)"
+echo "SLURM_JOB_ID is ${SLURM_JOB_ID}"
+echo "SLURM_ARRAY_JOB_ID is ${SLURM_ARRAY_JOB_ID}"
+echo "SLURM_ARRAY_TASK_ID is ${SLURM_ARRAY_TASK_ID}"
+
+sleep 10
