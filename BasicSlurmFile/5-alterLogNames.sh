3d70815683bbdc0d5cfe801cfd0645c80838864b
James Allsopp <j.allsopp@bham.ac.uk>
2024-02-14 10:25:55 +0000

    Changed logging path

--- BasicSlurmFile/5-alterLogNames.sh
+++ BasicSlurmFile/5-alterLogNames.sh
@@ -4,7 +4,7 @@
 #SBATCH --account vjgo8416-training24
 #SBATCH --time 1:0:0
 #SBATCH --job-name "alterLogNamesTest"
-#SBATCH --output=/bask/homes/a/allsoppj/RFI-27112023/basicSlurmScripts/BasicSlurmFile/LogStore/alterLogNames-%j.out  #absolute path protects you from changes in where sbatch is called from
+#SBATCH --output=/bask/homes/a/allsoppj/basicSlurmScripts/BasicSlurmFile/LogStore/alterLogNames-%j.out  #absolute path protects you from changes in where sbatch is called from
 
 module purge
 module load baskerville
